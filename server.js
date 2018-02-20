c398737ff5f85b6d4e112e069f8362e3a92f6d9d
CSreeram <36541636+CSreeram@users.noreply.github.com>
2018-02-20 11:27:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1',function(req,res){
+    res.send('article1 is executed');
+});
+app.get('/article2',function(req,res){
+    res.send('article2 is executed');
+});
+app.get('/article3',function(req,res){
+    res.send('article3 is executed');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
