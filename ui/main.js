418ee6145403b01cfafd56d141ab97780e5900f4
CSreeram <36541636+CSreeram@users.noreply.github.com>
2018-02-24 10:56:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft+='5px';
+    marginLeft=marginLeft+'5px';
     img.style.marginLeft=marginLeft+'px';
 }
 
